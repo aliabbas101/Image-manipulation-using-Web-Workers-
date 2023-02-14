6dd5fe5fa73310cdb13cdfbb50d6795059faaab1
aliabbas101 <31781103+aliabbas101@users.noreply.github.com>
2023-02-14 11:11:21 +0500

    Image manipulation on canvas

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-// Import stylesheets
-import './style.css';
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const image = new Image();
+image.crossOrigin = "Anonymous";
+console.log("hello world");
+image.src = 'https://raw.githubusercontent.com/aliabbas101/Image-manipulation-using-Web-Workers-/main/highres.jpg';
+image.onload = () => {
+  // rendering image on canvas
+  context.drawImage(image, 0,0);
+
+  const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+  const data = imageData.data;
+
+  // Manipulating image by looping on each pixel
+  for (let i = 0; i < data.length; i += 4) {
+    data[i] = 255 - data[i]; // invert the red channel
+  }
+
+  context.putImageData(imageData, 0, 0);
+};
